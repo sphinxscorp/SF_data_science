d4a902835fc054e646c70a53e07f9c6f055c4d1b
sphinxscorp <evgeny_dmitruk@mail.ru>
2022-02-13 00:41:41 +0300

    edit game

--- project_0/game.py
+++ project_0/game.py
@@ -5,7 +5,7 @@
 import numpy as np
 
 
-def random_predict(number: int = 1) -> int:
+def optimal_predict(number: int = 1) -> int:
     """Угадываем число меньше чем за 20 попыток
 
     Args:
@@ -15,32 +15,33 @@ def random_predict(number: int = 1) -> int:
         int: Число попыток
     """
     min = 1
-max = 101
+    max = 101
 
-number = np.random.randint(min, max)
+    number = np.random.randint(min, max)
 
-count = 0
+    count = 0
 
-while True:
-    count+=1
-    mid = round((min+max) / 2)
+    while True:
+        count+=1
+        mid = round((min+max) / 2)
     
-    if mid > number:
-        max = mid
+        if mid > number:
+            max = mid
     
-    elif mid < number:
-        min = mid
+        elif mid < number:
+            min = mid
 
-    else:
-        print(f"Компьютер угадал число за {count} попыток. Это число {number}")
-        break #конец игры выход из цикла
+        else:
+            print(f"Компьютер угадал число за {count} попыток. Это число {number}")
+            break #конец игры выход из цикла
+    return count
 
 
-def score_game(random_predict) -> int:
+def score_game(optimal_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        optimal_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
@@ -50,7 +51,7 @@ def score_game(random_predict) -> int:
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(optimal_predict(number))
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
@@ -59,4 +60,4 @@ def score_game(random_predict) -> int:
 
 if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+    score_game(optimal_predict)
