071d9ed3ce2ea51fd4009b5ebef43f0997bc56a4
sphinxscorp <evgeny_dmitruk@mail.ru>
2022-02-13 23:16:41 +0300

    edit game

--- project_0/game.py
+++ project_0/game.py
@@ -5,34 +5,56 @@
 import numpy as np
 
 
-def optimal_predict(number: int = 1) -> int:
-    """Угадываем число меньше чем за 20 попыток
-
+def optimal_predict_random(number: int = 1) -> int:
+    """Компьютер загадывает и угадывает число меньше чем за 20 попыток
+    
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
 
     Returns:
         int: Число попыток
     """
+    
+    number = np.random.randint(1, 101) #рандомайзер для загадывания чисел компьютером
+           
     min = 1
     max = 101
-
-    number = np.random.randint(min, max)
-
     count = 0
-
-    while True:
-        count+=1
-        mid = round((min+max) / 2)
     
-        if mid > number:
-            max = mid
-    
-        elif mid < number:
+    mid = (min + max) // 2
+    while number != mid:
+        count += 1
+        if number > mid:
             min = mid
-
         else:
-            break #конец игры выход из цикла
+            max = mid
+        mid = (min + max) // 2
+    print(f'Загаданное число {number} угадано за {count} попыток') #вывод результата для самостоятельной работы функции
+    return count
+
+
+def optimal_predict(number: int = 1) -> int:
+    """Угадываем число меньше чем за 20 попыток
+    
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+           
+    min = 1
+    max = 101
+    count = 0
+    
+    mid = (min + max) // 2
+    while number != mid:
+        count += 1
+        if number > mid:
+            min = mid
+        else:
+            max = mid
+        mid = (min + max) // 2
     return count
 
 
@@ -46,17 +68,16 @@ def score_game(optimal_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
-
+    
     for number in random_array:
         count_ls.append(optimal_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
     return score
 
 
-if __name__ == "__main__":
-    # RUN
+if __name__ == '__main__':
     score_game(optimal_predict)
